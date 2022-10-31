37c88438050085848138a6950eca8f24868d56d3
Светлана Горина <arhifantazium@gmail.com>
2022-10-31 20:07:19 +0300

    Added beautiful fixes

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,3 +1,48 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+    slug = models.SlugField(max_length=50)
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+    
+    def __str__(self):
+        return self.name
+    
+    @classmethod
+    def get_default_pk(cls):
+        obj, created = cls.objects.get_or_create(name='No category')
+        return obj.pk
+
+
+class Genre(models.Model):
+    name = models.CharField(max_length=50)
+    slug = models.SlugField(max_length=50)
+
+    class Meta:
+        verbose_name = 'Жанр'
+        verbose_name_plural = 'Жанры'
+    
+    def __str__(self):
+        return self.name
+
+
+class Title(models.Model):
+    name = models.CharField(max_length=150)
+    year = models.PositiveSmallIntegerField()
+    category = models.ForeignKey(
+        Category, 
+        on_delete=models.SET_DEFAULT,
+        default = Category.get_default_pk
+        )
+    genre = models.ManyToManyField(Genre)
+    
+    class Meta:
+        verbose_name = 'Произведение'
+        verbose_name_plural = 'Произведения'
+    
+    def __str__(self):
+        return self.name
