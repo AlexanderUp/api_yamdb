afadba8a504a08c82f04da344c301a4097b1bb64
Светлана Горина <arhifantazium@gmail.com>
2022-11-03 18:47:47 +0300

    002 updated

--- api_yamdb/reviews/management/commands/import_csv.py
+++ api_yamdb/reviews/management/commands/import_csv.py
@@ -1,14 +1,14 @@
 from django.core.management.base import BaseCommand
 from reviews.models import Category, Genre, Title
-import csv
+import sqlite3
 from pathlib import Path
+from django.db.utils import IntegrityError
+import pandas
 
 DIR_PATH = Path.cwd()
-print(DIR_PATH)
-partial_path = Path(DIR_PATH, 'static', 'data', 'example.csv')
+
 MODELS_DICT = {'category': Category,
                'genre': Genre,
-               'genre_title': Genre,
                'titles': Title,
                }
 
@@ -18,23 +18,15 @@ class Command(BaseCommand):
 
     def add_arguments(self, parser):
         parser.add_argument(
-            'filename',
+            'tablename',
             type=str,
-            help=u'Имя csv файла')
+            help=u'Название таблицы')
 
     def handle(self, *args, **kwargs):
-        file_key = kwargs['filename']
-        file = file_key + '.csv'     
-        full_path = Path(partial_path.with_name(file))
-        model_name = MODELS_DICT[file_key]
-
-        with open(full_path, encoding='utf-8') as r_file:
-            file_reader = csv.reader(r_file, delimiter=",")
-            count = 0
-            for row in file_reader:
-                if count == 0:
-                    first_row = row
-                else:
-                    keyargs = dict(zip(first_row, row))
-                    model_name.objects.create(**keyargs)
-                count += 1
+        table_name = kwargs['tablename']
+        app, file_name = table_name.split('_', 1)
+        file = file_name + '.csv'
+        full_path = Path(DIR_PATH, 'static', 'data', file)
+        conn = sqlite3.connect('db.sqlite3')
+        df = pandas.read_csv(full_path)
+        df.to_sql(table_name, conn, if_exists='append', index=False)
