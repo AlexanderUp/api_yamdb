ef43a9c038eee6e635f2839a0b41b9fa4af47798
AlexanderUp <alexander.uperenk0@yandex.ru>
2022-11-07 15:21:08 +0300

    Title serializer intermediate commit

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -1,18 +1,16 @@
 #!/api_yamdb/api_yamdb/api/permissions.py
 """All permissions."""
 from rest_framework import permissions
-from rest_framework.permissions import BasePermission
 
 
 class IsAdminOrReadOnly(permissions.BasePermission):
     message = 'Пользователь не является администратором!'
 
     def has_permission(self, request, view):
+        is_admin = False
         if not request.user.is_anonymous:
-            admin = request.user.role == 'admin'
-        else:
-            admin = False
-        if request.method in permissions.SAFE_METHODS or admin:
+            is_admin = request.user.role == 'admin'
+        if request.method in permissions.SAFE_METHODS or is_admin:
             return True
         return False
 
--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -1,46 +1,100 @@
-#!/api_yamdb/api_yamdb/api/serializers.py
 """All Serializers."""
+import sys
 from datetime import datetime as dt
 
 from django.core.validators import MaxValueValidator, MinValueValidator
 from django.shortcuts import get_object_or_404
 from rest_framework import serializers
 
+from .validators import (validate_category_slug_existance,
+                         validate_genre_slug_existance)
+
 from reviews.models import Category, Comment, Genre, Review, Title  # isort:skip
-
-
-class GenreSerializer(serializers.ModelSerializer):
-
-    class Meta:
-        model = Genre
-        fields = ('name', 'slug')
-        lookup_field = 'slug'
-
-
-class TitleSerializer(serializers.ModelSerializer):
-
-    class Meta:
-        model = Title
-        fields = ('name', 'year', 'description', 'category')
-
-    def validate(self, data):
-        if data['year'] > dt.now().year:
-            raise serializers.ValidationError(
-                'Год выпуска не может быть больше текущего!'
-            )
-        return data
 
 
 class CategorySerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Category
-        fields = ('name', 'slug')
         lookup_field = 'slug'
+        fields = ('name', 'slug')
 
 
-class TitlePostSerializer(serializers.ModelSerializer):
-    pass
+class CategoryLightSerializer(CategorySerializer):
+
+    class Meta(CategorySerializer.Meta):
+        extra_kwargs = {
+            'name': {
+                "required": False
+            },
+            'slug': {
+                'validators': [validate_category_slug_existance],
+            }
+        }
+
+
+class GenreSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Genre
+        lookup_field = 'slug'
+        fields = ('name', 'slug')
+
+
+class GenreLightSerializer(GenreSerializer):
+
+    class Meta(GenreSerializer.Meta):
+        model = Genre
+        lookup_field = 'slug'
+        fields = ('name', 'slug')
+        extra_kwargs = {
+            'name': {
+                "required": False
+            },
+            'slug': {
+                'validators': [validate_genre_slug_existance],
+            }
+        }
+
+
+class TitleSerializer(serializers.ModelSerializer):
+    genre = GenreLightSerializer(many=True)
+    category = CategoryLightSerializer()
+
+    class Meta:
+        model = Title
+        fields = ('id', 'name', 'year', 'description', 'genre', 'category')
+
+    def validate_year(self, value):
+        if value > dt.now().year:
+            raise serializers.ValidationError(
+                'Год выпуска не может быть больше текущего!'
+            )
+        return value
+
+    def validate(self, validated_data):
+        name = validated_data.get("name")
+        category = validated_data.get("category")
+        category_slug = category.get("slug")
+
+        if Title.objects.filter(name=name, category__slug=category_slug).exists():
+            raise serializers.ValidationError("Title already exists.")
+        return validated_data
+
+    def create(self, validated_data):
+        category_data = validated_data.pop("category")
+        category_slug = category_data.get("slug")
+        category = Category.objects.get(slug=category_slug)
+
+        genre_data = validated_data.pop("genre")
+        genre_slug_list = [genre.get("slug") for genre in genre_data]
+        genre_list = Genre.objects.filter(slug__in=genre_slug_list)
+
+        title = Title.objects.create(**validated_data)
+        title.category = category
+        title.genre.set(genre_list)
+        title.save()
+        return title
 
 
 class ReviewSerializer(serializers.ModelSerializer):
